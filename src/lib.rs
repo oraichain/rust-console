119360d698e5c4d527285159a468ede195c13ebd
Pham Tu <tubackkhoa@gmail.com>
2023-11-20 10:34:21 +0700

    update code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+pub mod mock;
+
 #[macro_export]
 macro_rules! log {
     ($($a:tt)*) => {
-        cosmwasm_std::testing::mock_dependencies().api.debug(&cosmwasm_schema::schemars::_serde_json::to_string_pretty(&($($a)*)).unwrap())
+        &cosmwasm_schema::schemars::_serde_json::to_string_pretty(&($($a)*)).unwrap()
     };
 }
